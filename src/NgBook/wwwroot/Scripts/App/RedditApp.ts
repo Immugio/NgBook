13baeb7009a173aa2f88222d1cbab7d97a19e656
Immugio <janmikeska@googlemail.com>
2016-04-08 14:20:39 +0200

    Adding and sorting articles

--- src/NgBook/wwwroot/Scripts/App/RedditApp.ts
+++ src/NgBook/wwwroot/Scripts/App/RedditApp.ts
@@ -6,33 +6,38 @@ class Article {
     constructor(public title: string, public link: string, public votes?: number) {
         this.votes = votes || 0;
     }
+
+    public domain(): string {
+        try {
+            let link = this.link.split("//")[1];
+            return link.split("/")[0];
+        }
+        catch(ex) {
+            return null;
+        }
+    }
 }
 
 @Component({
     selector: "reddit-article",
     host: { "class": "row" },
+    inputs: ["article"],
     templateUrl: "reddit-article.html"
 })
 class RedditArticle {
 
-    public votes: number;
-    public title: string;
-    public link: string;
+    public article: Article;
 
-    constructor() {
-        this.title = "Angular 2";
-        this.link = "http://angular.io";
-        this.votes = 10;
-    }
+    constructor() { }
 
     public voteUp($event: Event) {
         $event.preventDefault();
-        this.votes += 1;
+        this.article.votes += 1;
     }
 
     public voteDown($event: Event) {
         $event.preventDefault();
-        this.votes -= 1;
+        this.article.votes -= 1;
     }
 }
 
@@ -43,10 +48,25 @@ class RedditArticle {
 })
 class RedditApp {
 
-    constructor() { }
+    public articles: Article[];
+
+    constructor() {
+        this.articles = [
+            new Article("Angular 2", "http://angular.io", 3),
+            new Article("Fullstack", "http://fullstack.io", 2),
+            new Article("Angular Homepage", "http://angular.io", 1)
+        ];
+    }
 
     public addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
         console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+        this.articles.push(new Article(title.value, link.value, 0));
+        title.value = "";
+        link.value = "";
+    }
+
+    public sortedArticles(): Article[] {
+        return this.articles.sort((a, b) => { return b.votes - a.votes });
     }
 }
 
