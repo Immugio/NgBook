5410344ada3b1a9e6d59ec7b2a972e9be0001bfd
Immugio <janmikeska@googlemail.com>
2016-04-12 14:50:29 +0200

    Another test app set up

---
+++ src/NgBook/wwwroot/Scripts/TestApp/TestApp.ts
@@ -0,0 +1,15 @@
+﻿import { bootstrap } from "angular2/platform/browser";
+import { Component } from "angular2/core"
+
+@Component({
+    selector: "test-app",
+    templateUrl: "test-app.html"
+})
+class TestApp {
+
+    constructor() {
+
+    }
+}
+
+bootstrap(TestApp);
